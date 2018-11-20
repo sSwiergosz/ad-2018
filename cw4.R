09dff56f2c4ff7494a4006827f97bbf89b2a0ce8
sSwiergosz <szymon.swiergosz@gmail.com>
2018-11-20 20:13:34 +0100

    Dodanie zadania 4

--- cw4.R
+++ cw4.R
@@ -30,3 +30,16 @@ data.set <- data.frame(x, y)
 model.lm <- lm(y ~ x, data = data.set)
 summary(model.lm)
 predict(model.lm, newdata = data.frame(x = 75000), se = TRUE) # Prediction
+
+#zad4
+library(data.table)
+price <- c(300, 250, 400, 550, 317, 389, 425, 289, 389, 559)
+rooms <- c(3, 3, 4, 5, 4, 3, 6, 3, 4, 5)
+data.city <- data.table(x=rooms, y=price)
+
+ggplot(data.city, aes(x=data.city$x, y=data.city$y)) + geom_point() + 
+  labs(x="Rooms", y="Price") +
+  geom_smooth(method = "lm", se=FALSE, formula = y ~ x^2)
+model.lm <- lm(y ~ x, data = data.city)
+summary(model.lm)
+predict(model.lm, newdata = data.frame(x = 2), se = TRUE)$fit # Prediction

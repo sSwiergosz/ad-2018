d2f09151d5a727e0170f9a004a81894929784714
sSwiergosz <szymon.swiergosz@gmail.com>
2018-11-20 11:36:18 +0100

    Poprawienie zad1 i dodanie zad3 z cw4

--- cw4.R
+++ cw4.R
@@ -4,15 +4,13 @@ library(ggplot2)
 library(gridExtra)
 
 #Zad1
-
-curve(x^2)
-
 cars %>% 
-  ggplot(aes(x = speed, y = dist)) + geom_point() + geom_smooth(method = "lm",se=FALSE, formula = y ~ x^2) 
+  ggplot(aes(x = speed, y = dist)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se=FALSE, formula = y ~ x^2) + 
+  geom_smooth(method = "lm", se=FALSE, formula = y ~ x + I(x^2), color = "red")
 
-=
 #zad2
-
 library(MASS)
   
 p1 = hills %>% 
@@ -21,3 +19,14 @@ p2 = hills %>%
   ggplot(aes(x = time , y = climb)) + geom_point() + geom_smooth(method = "lm",se=FALSE, formula = y ~ x^2)
 
 grid.arrange(p1,p2)
+
+#zad3
+install.packages("UsingR", dependencies=TRUE)
+library(UsingR)
+
+x <- homedata$y1970
+y <- homedata$y2000
+data.set <- data.frame(x, y)
+model.lm <- lm(y ~ x, data = data.set)
+summary(model.lm)
+predict(model.lm, newdata = data.frame(x = 75000), se = TRUE) # Prediction
